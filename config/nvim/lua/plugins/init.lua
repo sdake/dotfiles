e127813a8151a4f8bee93c9d2037c872f841d2d3
Steven Dake <steven.dake@gmail.com>
2025-01-27 13:39:23 -0700

    Introduce the editor neovim

---
+++ config/nvim/lua/plugins/init.lua
@@ -0,0 +1,43 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+ 	"nvim-treesitter/nvim-treesitter",
+ 	opts = {
+ 		ensure_installed = {
+ 			"vim", "lua", "vimdoc", "html", "css"
+ 		},
+ 	},
+ },
+  "nvim-lua/plenary.nvim",
+  { "nvim-tree/nvim-web-devicons", lazy = true },
+
+  {
+    "nvchad/ui",
+    config = function()
+      require "nvchad" 
+    end
+  },
+
+  {
+    "nvchad/base46",
+    lazy = true,
+    build = function()
+      require("base46").load_all_highlights()
+    end,
+ },
+
+ "nvchad/volt",
+}
