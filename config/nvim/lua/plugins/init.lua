cbb2e4d8a15c896b70b5b2840cab921d2ff5b1f4
Steven Dake <steven.dake@gmail.com>
2025-01-28 07:08:48 -0700

    Reformat init.lua in neovim

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -2,33 +2,37 @@ return {
   {
     "stevearc/conform.nvim",
     -- event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
+    opts = require("configs.conform"),
   },
 
   -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
     config = function()
-      require "configs.lspconfig"
+      require("configs.lspconfig")
     end,
   },
 
   {
- 	"nvim-treesitter/nvim-treesitter",
- 	opts = {
- 		ensure_installed = {
- 			"vim", "lua", "vimdoc", "html", "css"
- 		},
- 	},
- },
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+      },
+    },
+  },
   "nvim-lua/plenary.nvim",
   { "nvim-tree/nvim-web-devicons", lazy = true },
 
   {
     "nvchad/ui",
     config = function()
-      require "nvchad" 
-    end
+      require("nvchad")
+    end,
   },
 
   {
@@ -37,7 +41,7 @@ return {
     build = function()
       require("base46").load_all_highlights()
     end,
- },
+  },
 
- "nvchad/volt",
+  "nvchad/volt",
 }
